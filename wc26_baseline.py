deacd13babf7885376b9c94626c240c2b6cc683a
Kamran Huseynli <k.huseynli@ufaz.az>
2026-06-07 22:19:48 +0400

    feat: data to use chosen, advanced EDA and feature engineering needed

--- wc26_baseline.py
+++ wc26_baseline.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+"""
+# checking if combining is needed for fifa rankings
 ranking_files = [
     "fifa_ranking_2022-10-06.csv",
     "fifa_ranking_2023-07-20.csv",
@@ -16,3 +18,14 @@ rankings.to_csv("fifa_rankings_combined.csv", index=False)
 print(rankings.shape)
 print(rankings["rank_date"].unique())
 print(rankings.head())
+"""
+
+data_files = [
+    "fifa_rankings.csv",
+    "world_cup.csv",
+    "results.csv",
+    "shootouts.csv",
+    "goalscorers.csv",
+    "former_names.csv",
+    "matches_1930_2022.csv" #just in case for consistency with past wc matches
+]
