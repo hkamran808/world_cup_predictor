c9dc55ef67cbb4539d57197f52907ea7cea8c50b
Kamran Huseynli <k.huseynli@ufaz.az>
2026-06-29 21:18:21 +0400

    Planning last phase of project, and orginizing github for now

---
+++ src/tournament/group_stage.py
@@ -0,0 +1,14 @@
+def generate_group_matches(teams):
+    teams = [
+
+    ]
+
+"""
+last phase of project (github done): 
+
+1) finishing streamlit UI
+2) adding all the additional features for decoration
+3) deploying via docker
+4) orginizing readme.md
+5) adding to cv & linkedin
+"""
